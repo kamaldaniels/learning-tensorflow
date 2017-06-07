4b539af2541ae7115c398cf79ab4d01a98ced3c2
Kamal <kamaldaniels@Kamals-MacBook-Pro.local>
2017-06-07 13:08:18 +0100

    added new arguments and logging of model

--- mnist_softmax_cnn.py
+++ mnist_softmax_cnn.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 import sys
 
 import tensorflow as tf
@@ -29,6 +30,10 @@ def max_pool_2x2(x):
 
 
 def main(_):
+    if tf.gfile.Exists(FLAGS.log_dir):
+        tf.gfile.DeleteRecursively(FLAGS.log_dir)
+    tf.gfile.MakeDirs(FLAGS.log_dir)
+
     # data
     mnist = input_data.read_data_sets(FLAGS.data_dir, one_hot=True)
 
@@ -68,20 +73,25 @@ def main(_):
     cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_conv))
     train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 
+    saver = tf.train.Saver()
     sess = tf.InteractiveSession()
     tf.global_variables_initializer().run()
+
     correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
     sess.run(tf.global_variables_initializer())
 
     # train
-    for i in range(20000):
+    for i in range(FLAGS.max_steps):
         batch = mnist.train.next_batch(50)
 
-        if i % 100 == 0:
+        if i % 100 == 0 or (i + 1) == FLAGS.max_steps:
             train_accuracy = accuracy.eval(feed_dict={
                 x: batch[0], y_: batch[1], keep_prob: 1.0})
             print("step %d, training accuracy %g" % (i, train_accuracy))
+
+            checkpoint_file = os.path.join(FLAGS.log_dir, 'model.ckpt')
+            saver.save(sess, checkpoint_file, global_step=i)
 
         train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
 
@@ -95,6 +105,17 @@ if __name__ == '__main__':
                         type=str,
                         default='/tmp/tensorflow/mnist/input_data',
                         help='Directory for storing input data')
-
+    parser.add_argument(
+        '--max_steps',
+        type=int,
+        default=2000,
+        help='Number of steps to run trainer.'
+    )
+    parser.add_argument(
+        '--log_dir',
+        type=str,
+        default='/tmp/tensorflow/mnist/logs/fully_connected_feed',
+        help='Directory to put the log data.'
+    )
     FLAGS, unparsed = parser.parse_known_args()
     tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
