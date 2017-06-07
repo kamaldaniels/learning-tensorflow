1014cc78797dc08298ead97817e318fabb25624d
Kamal <kamaldaniels@Kamals-MacBook-Pro.local>
2017-06-07 13:10:45 +0100

    update default steps

--- mnist_softmax_cnn.py
+++ mnist_softmax_cnn.py
@@ -108,7 +108,7 @@ if __name__ == '__main__':
     parser.add_argument(
         '--max_steps',
         type=int,
-        default=2000,
+        default=20000,
         help='Number of steps to run trainer.'
     )
     parser.add_argument(
